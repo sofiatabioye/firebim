e2c319f90bf2ce2fd70c195ed8b42f6d697e63df
sofiatabioye <sofiat.abioye@previsico.comgit config --global user.email sofiat.abioye@previsico.com>
2022-02-08 22:48:25 +0000

    change username

--- src/views/pages/users/userData.js
+++ src/views/pages/users/userData.js
@@ -1,6 +1,6 @@
 const assetData = [
   {id: 0, name: 'Adeyinka Adeleke', email: 'adeyinka.adeleke@firebim.com', role: 'designer', project: 'Virtual project community center'},
   {id: 1, name: 'Omolara Arawomo', email: 'omolara.arawomo@firebim.com', role: 'manufacturer', project: 'Training center'},
-  {id: 2, name: 'Saheed Ajayi', email: 'saheed.alaka@gmail.com', role: 'installation', project: 'School Cafetaria'},
+  {id: 2, name: 'Saheed Ajayi', email: 'saheed.ajayi@gmail.com', role: 'installation', project: 'School Cafetaria'},
 ]
 export default assetData
