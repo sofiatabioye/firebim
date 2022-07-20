97c636430a8f8798573def03884c3b16157de4ab
sofiatabioye <sofiat.abioye@previsico.comgit config --global user.email sofiat.abioye@previsico.com>
2022-07-20 14:23:02 +0100

    update firerating

--- src/views/pages/viewer/fireRating.js
+++ src/views/pages/viewer/fireRating.js
@@ -1,30 +1,70 @@
 
 
 
-export const calculateFireRating = (height)  => {
+export const calculateFireRating = (type, height)  => {
    let rating;
    let sprinkler;
    let dssDisplay;
    let description;
    let sprinklerText;
- 
+   let exposureMethod;
+   height = Number(height);
    switch(true) {
-       case (height <= 5):
+      case(type === 'Walls' && height <=5):
+            rating = 30;
+            sprinkler= 'No';
+            dssDisplay= 'REI 30';
+            description = '';
+            sprinklerText = '';
+            exposureMethod = 'Exposed faces';
+            return {rating, sprinkler, dssDisplay, description, sprinklerText, exposureMethod};
+       case(type === 'compartmentWalls' && height <=5):
            rating = 30;
            sprinkler= 'No';
-           dssDisplay= 'E 30';
-           description = 'The Approved Document B1 requires that this wall must have a fire rating of 30 min because the depth of basement was less than 5m.\n \n Note: The surface lining of wall and ceiling should meet a classification of C-s3, d2 if the room is more than 4m2 and should meet a classification of D-s3, d2 if the room is 4m2';
+           dssDisplay= 'REI 30';
+           description = '';
            sprinklerText = '';
-           return {rating, sprinkler, dssDisplay, description, sprinklerText};
+           exposureMethod = 'Each side separately';
+           return {rating, sprinkler, dssDisplay, description, sprinklerText, exposureMethod};
 
-       case (height > 5 && height<=11):
-           rating = '60 mins';
+       case (type=== 'Floors' && height <= 5):
+           rating = 30;
            sprinkler= 'No';
-           dssDisplay= 'E 60';
+           dssDisplay= 'REI 30';
+           description = 'The approved document B requires that the surface lining of an internal wall and ceiling should meet a classification of C-s3, d2 if the room is more than 4m2 and should meet a classification of D-s3, d2 if the room is less than 4m2 and B-s3, d2 for an escape route';
+           sprinklerText = '';
+           exposureMethod = 'From the underside';
+           return {rating, sprinkler, dssDisplay, description, sprinklerText, exposureMethod};
+       case (type === 'Walls' && height > 5 && height<=11):
+           rating = 60;
+           sprinkler= 'No';
+           dssDisplay= 'REI 60';
            description = `The Approved Document B1 requires that this wall must have a fire rating of 60 mins because the height of the top floor is up to 11m.\n \n 
                          Note: The surface lining of wall and ceiling should meet a classification of C-s3, d2 if the room is more than 4m2 and should meet a classification of D-s3, d2 if the room is 4m2`;
            sprinklerText = '';
-           return {rating, sprinkler, dssDisplay, description, sprinklerText}
+           exposureMethod = 'Exposed faces';
+           return {rating, sprinkler, dssDisplay, description, sprinklerText, exposureMethod}
+        
+       case (type === 'Floors' && height > 5 && height<=11):
+           rating = 60;
+           sprinkler= 'No';
+           dssDisplay= 'REI 60';
+           description = `The Approved Document B1 requires that this wall must have a fire rating of 60 mins because the height of the top floor is up to 11m.\n \n 
+                         Note: The surface lining of wall and ceiling should meet a classification of C-s3, d2 if the room is more than 4m2 and should meet a classification of D-s3, d2 if the room is 4m2`;
+           sprinklerText = '';
+           exposureMethod = 'From the underside';
+           return {rating, sprinkler, dssDisplay, description, sprinklerText, exposureMethod}
+
+        case (type === 'compartmentWalls' && height > 5 && height<=11):
+            rating = 60;
+            sprinkler= 'No';
+            dssDisplay= 'REI 60';
+            description = `The Approved Document B1 requires that this wall must have a fire rating of 60 mins because the height of the top floor is up to 11m.\n \n 
+                          Note: The surface lining of an internal wall and ceiling should meet a classification of C-s3, d2 if the room is more than 4m2 and should meet a classification of D-s3, d2 if the room is less than 4m2 and B-s3, d2 for an escape route`;
+            sprinklerText = '';
+            exposureMethod = 'Each side separately';
+            return {rating, sprinkler, dssDisplay, description, sprinklerText, exposureMethod}
+       
        case (height > 11 && height<=18):
            rating = 60;
            sprinkler= 'Yes';
@@ -54,8 +94,5 @@ export const calculateFireRating = (height)  => {
            sprinklerText = 'N/A';
            return {rating, sprinkler, dssDisplay, description, sprinklerText}
    }
-//    return {
-//     rating, sprinkler, dssDisplay, description, sprinklerText
-//    }
 
 }
